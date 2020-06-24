24cecb0362add9c29e64fc5c2c8c9e21befaab37
Robert Hernandez <rahernande96@gmail.com>
2020-06-24 19:22:08 -0400

    correciones de equivalentes

--- database/seeds/FoodGroupEquivalentsTableSeeder.php
+++ database/seeds/FoodGroupEquivalentsTableSeeder.php
@@ -12,182 +12,144 @@ class FoodGroupEquivalentsTableSeeder extends Seeder
      */
     public function run()
     {
+        //VERDURAS
         FoodGroupEquivalent::create([
             'group_id'      => 1,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 2,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 3,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 4,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 5,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 6,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 7,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 8,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 9,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 10,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 11,
-            'energy'        => 0,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 0
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 12,
-            'energy'        => 140,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 20
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 13,
-            'energy'        => 60,
-            'protein'       => 0,
-            'lipids'        => 0,
-            'carbohydrates' => 15
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 14,
-            'energy'        => 120,
-            'protein'       => 8,
-            'lipids'        => 1,
-            'carbohydrates' => 20
-        ]);
-
-        FoodGroupEquivalent::create([
-            'group_id'      => 15,
             'energy'        => 25,
             'protein'       => 2,
             'lipids'        => 0,
             'carbohydrates' => 4
         ]);
+        //FRUTAS
+        FoodGroupEquivalent::create([
+            'group_id'      => 2,
+            'energy'        => 60,
+            'protein'       => 0,
+            'lipids'        => 0,
+            'carbohydrates' => 15
+        ]);
+        //CEREALES Y TUBÉRCULOS
+        FoodGroupEquivalent::create([
+            'group_id'      => 3,
+            'energy'        => 70,
+            'protein'       => 2,
+            'lipids'        => 0,
+            'carbohydrates' => 15
+        ]);
+        //CEREALES CON GRASA
 
         FoodGroupEquivalent::create([
-            'group_id'      => 16,
+            'group_id'      => 4,
+            'energy'        => 115,
+            'protein'       => 2,
+            'lipids'        => 5,
+            'carbohydrates' => 15
+        ]);
+        //LEGUMINOSAS
+        FoodGroupEquivalent::create([
+            'group_id'      => 5,
+            'energy'        => 120,
+            'protein'       => 8,
+            'lipids'        => 1,
+            'carbohydrates' => 20
+        ]);
+        //AOA MUY BAJO APORTE DE GRASA
+        FoodGroupEquivalent::create([
+            'group_id'      => 6,
+            'energy'        => 40,
+            'protein'       => 7,
+            'lipids'        => 1,
+            'carbohydrates' => 0
+        ]);
+        //AOA BAJO APORTE DE GRASA
+        FoodGroupEquivalent::create([
+            'group_id'      => 7,
+            'energy'        => 55,
+            'protein'       => 7,
+            'lipids'        => 3,
+            'carbohydrates' => 0
+        ]);
+        //AOA MODERADO APORTE DE GRASA
+        FoodGroupEquivalent::create([
+            'group_id'      => 8,
             'energy'        => 75,
             'protein'       => 7,
             'lipids'        => 5,
             'carbohydrates' => 0
         ]);
-
+        //AOA ALTO APORTE DE GRASA
         FoodGroupEquivalent::create([
-            'group_id'      => 17,
+            'group_id'      => 9,
+            'energy'        => 100,
+            'protein'       => 7,
+            'lipids'        => 8,
+            'carbohydrates' => 0
+        ]);
+        //LECHE
+        FoodGroupEquivalent::create([
+            'group_id'      => 10,
+            'energy'        => 95,
+            'protein'       => 9,
+            'lipids'        => 2,
+            'carbohydrates' => 12
+        ]);
+        //LECHE SEMIDESCREMADA
+        FoodGroupEquivalent::create([
+            'group_id'      => 11,
+            'energy'        => 110,
+            'protein'       => 9,
+            'lipids'        => 4,
+            'carbohydrates' => 12
+        ]);
+        //LECHE ENTERA
+        FoodGroupEquivalent::create([
+            'group_id'      => 12,
+            'energy'        => 150,
+            'protein'       => 9,
+            'lipids'        => 8,
+            'carbohydrates' => 12
+        ]);
+        //LECHE CON AZÚCAR
+        FoodGroupEquivalent::create([
+            'group_id'      => 13,
             'energy'        => 200,
             'protein'       => 8,
             'lipids'        => 5,
             'carbohydrates' => 30
         ]);
-
-        // FoodGroupEquivalent::create([
-        //     'group_id'      => 18,
-        //     'energy'        => 45,
-        //     'protein'       => 0,
-        //     'lipids'        => 5,
-        //     'carbohydrates' => 0
-        // ]);
-
-        // FoodGroupEquivalent::create([
-        //     'group_id'      => 19,
-        //     'energy'        => 70,
-        //     'protein'       => 3,
-        //     'lipids'        => 5,
-        //     'carbohydrates' => 3
-        // ]);
-
-        // FoodGroupEquivalent::create([
-        //     'group_id'      => 20,
-        //     'energy'        => 40,
-        //     'protein'       => 0,
-        //     'lipids'        => 0,
-        //     'carbohydrates' => 10
-        // ]);
-
-        // FoodGroupEquivalent::create([
-        //     'group_id'      => 21,
-        //     'energy'        => 85,
-        //     'protein'       => 0,
-        //     'lipids'        => 5,
-        //     'carbohydrates' => 10
-        // ]);
-
-        // FoodGroupEquivalent::create([
-        //     'group_id'      => 22,
-        //     'energy'        => 0,
-        //     'protein'       => 0,
-        //     'lipids'        => 0,
-        //     'carbohydrates' => 0
-        // ]);
-
+        //ACEITES Y GRASAS
+        FoodGroupEquivalent::create([
+            'group_id'      => 14,
+            'energy'        => 45,
+            'protein'       => 0,
+            'lipids'        => 5,
+            'carbohydrates' => 0
+        ]);
+        //ACEITES Y GRASAS CON PROTEÍNA
+        FoodGroupEquivalent::create([
+            'group_id'      => 15,
+            'energy'        => 70,
+            'protein'       => 3,
+            'lipids'        => 5,
+            'carbohydrates' => 3
+        ]);
+        //AZÚCARES SIN GRASA
+        FoodGroupEquivalent::create([
+            'group_id'      => 16,
+            'energy'        => 40,
+            'protein'       => 0,
+            'lipids'        => 0,
+            'carbohydrates' => 10
+        ]);
+        //AZÚCARES CON GRASA
+        FoodGroupEquivalent::create([
+            'group_id'      => 17,
+            'energy'        => 85,
+            'protein'       => 0,
+            'lipids'        => 5,
+            'carbohydrates' => 10
+        ]);
         
+
     }
 }
